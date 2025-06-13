4cf42e14558c349167786dfae0280651c793b0c0
leah-1ee <dltjddms1028@gmail.com>
2025-06-13 21:05:06 +0900

    Title: 점수대 파악하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250613/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,14 @@
+lst = list(map(int, input().split()))
+count = [0]*10
+
+# 점수대 세기 
+for score in lst:
+    if score == 0:
+        break
+
+    elif score > 10:
+        n = (score//10) -1
+        count[n] += 1
+
+for i, j in zip(range(100,0,-10), reversed(count)):
+    print(f"{i} - {j}")
