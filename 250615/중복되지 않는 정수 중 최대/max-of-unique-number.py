be630ef87d0d443dc550b675be74343a7e6b053b
leah-1ee <dltjddms1028@gmail.com>
2025-06-15 14:18:15 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250615/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ 250615/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -1,22 +1,6 @@
 n = int(input())
 nums = list(map(int, input().split()))
-flag = False
 
 # Please write your code here.
-# 카운트 배열 채우기 
-count = [0] * (n+1)
 
-for i in nums:
-    count[i] += 1
-
-# 뒤에서부터(큰 수부터 탐색), 중복되지 않은 수 출력
-for i in range(n,-1,-1):
-    if count[i] == 1:
-        print(i)
-        flag = True
-        break
-
-# 조건에 맞는 원소 존재하지 않음 
-if flag == False:
-    print("-1")
-    
+print(max([x for x in nums if nums.count(x)==1], default=-1)) 
