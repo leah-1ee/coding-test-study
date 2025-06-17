b4a5448ec066536de3e980ea401bae31ff36c5f4
leah-1ee <dltjddms1028@gmail.com>
2025-06-17 23:13:52 +0900

    Title: 배열의 평균, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250617/배열의 평균/ave-of-array.py
@@ -0,0 +1,13 @@
+arr = [list(map(int, input().split())) for _ in range(2)]
+
+# 행 평균
+row_avr = [round(sum(row)/4, 1) for row in arr]
+print(*row_avr)
+
+# 열 평균
+col_avr = [round(sum(col)/2, 1) for col in zip(*arr)]
+print(*col_avr)
+
+# 전체 평균
+print(round(sum(row_avr)/2, 1))
+
