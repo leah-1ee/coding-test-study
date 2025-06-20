477a131f8621ca1b5b639f95181bb9d8dd1b8b1d
leah-1ee <dltjddms1028@gmail.com>
2025-06-20 18:20:25 +0900

    Title: 아래로 사각형 채우기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250620/아래로 사각형 채우기/filling-rectangle-with-downflow.py
+++ 250620/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -1,16 +1,11 @@
 n = int(input())
 arr = []
-num = 1
 
+# i: 행 인덱스, j: 열 인덱스
 for i in range(n):
-    row = []
-    elem = num
-    for j in range(n):
-        row.append(elem)
-        elem += n
-        
+    row = [(j * n) + (i + 1) for j in range(n)]  # 한 줄에 바로 계산해서 넣기
     arr.append(row)
-    num += 1
 
+# 출력
 for row in arr:
     print(*row)
