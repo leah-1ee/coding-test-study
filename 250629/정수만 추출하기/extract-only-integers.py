cdf64fd2961cba20d2a776b86c39a770a263a5f6
leah-1ee <dltjddms1028@gmail.com>
2025-06-29 00:55:23 +0900

    Title: 정수만 추출하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250629/정수만 추출하기/extract-only-integers.py
+++ 250629/정수만 추출하기/extract-only-integers.py
@@ -4,10 +4,12 @@ for elem in a:
     if not elem.isdigit():
         idx = a.find(elem)
         a = a[:idx]
+        break
 
 for elem in b:
     if not elem.isdigit():
         idx = b.find(elem)
         b = b[:idx]
+        break
 
 print(int(a) + int(b))
