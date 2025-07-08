160774435a595816c54fe91d84c29ecfa8bd4a6b
leah-1ee <dltjddms1028@gmail.com>
2025-07-08 17:35:14 +0900

    Update ［PCCE 기출문제］ 10번 ／ 공원.py

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,3 +1,11 @@
+# park에서 -1만 있는 정사각형 찾기
+# 가장 큰 정사각형부터 시도
+
+# 1. 가장 큰 돗자리부터 park 전체에 시도
+# 2. 각 위치 [i, j]에서 size x size 가능한지 확인
+# 3. 가능하면 리턴, 안 되면 다음 크기로
+
+# 돗자리가 들어갈 수 있는지 확인
 def can_place(park, i, j, size):
     # i, j 를 시작점으로 size x size 가능?
     for x in range(i, i + size):
@@ -9,6 +17,7 @@ def can_place(park, i, j, size):
     return True
 
 def solution(mats, park):
+    # 가장 큰 정사각형부터 시도
     mats.sort(reverse=True)
 
     for size in mats:
