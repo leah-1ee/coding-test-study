04de0d142698173fbd87481cd2bd2cbbc75284e6
leah-1ee <dltjddms1028@gmail.com>
2025-07-13 13:51:52 +0900

    Title: 짝수의 합 n번 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250713/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a, b;
+    int sum;
+
+    scanf("%d", &n);
+
+    for (int i=0; i<n; i++){
+        scanf("%d %d", &a, &b);
+        for (int j=a; j<=b; j++){
+            
+            if (j%2==0){
+                sum += j;
+            }
+        }
+        printf("%d\n", sum);
+        sum=0;
+
+    }
+    return 0;
+}
