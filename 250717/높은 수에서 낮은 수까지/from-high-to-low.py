85c37297db013956b3eb0f309b6c09906a53d15a
leah-1ee <dltjddms1028@gmail.com>
2025-07-17 15:59:34 +0900

    Title: 높은 수에서 낮은 수까지, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250717/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 250717/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,4 +1,8 @@
 a, b = map(int, input().split())
 
-for i in range(b, a-1, -1):
-    print(i, end=" ")
+if (b>a):
+    for i in range(b, a-1, -1):
+        print(i, end=" ")
+else:
+    for i in range(a, b-1, -1):
+        print(i, end=" ")
