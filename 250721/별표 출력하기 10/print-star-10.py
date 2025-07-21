2dfa5592846ed5c0255bc9d2eccd514b637f7243
leah-1ee <dltjddms1028@gmail.com>
2025-07-21 15:45:35 +0900

    Title: 별표 출력하기 10, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250721/별표 출력하기 10/print-star-10.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n*2):
+    if (i%2 == 0):
+        for _ in range(i//2+1):
+            print("*", end=" ")
+
+    else:
+        for _ in range(n-(i-1)//2):
+            print("*", end=" ")
+
+    print()
