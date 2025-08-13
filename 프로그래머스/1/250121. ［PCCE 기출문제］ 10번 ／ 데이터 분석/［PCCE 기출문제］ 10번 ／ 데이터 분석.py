52ffdc6376e194e666f2a64feb1c4ac0956a2901
leah-1ee <dltjddms1028@gmail.com>
2025-08-13 23:52:20 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.10 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,42 +1,12 @@
 def solution(data, ext, val_ext, sort_by):
-    answer = []
+    table_map = {"code":0, "date":1, "maximum":2, "remain":3}
     
-
-    # 필터링
-    if ext == "code":
-        answer = [d for d in data if d[0] < val_ext]
-    elif ext == "date":
-        answer = [d for d in data if d[1] < val_ext]
-    elif ext == "maximum":
-        answer = [d for d in data if d[2] < val_ext]
-    elif ext == "remain":
-        answer = [d for d in data if d[3] < val_ext]
-
-    # 정렬
-    if sort_by == "code":
-        answer.sort(key=lambda x: x[0])
-    elif sort_by == "date":
-        answer.sort(key=lambda x: x[1])
-    elif sort_by == "maximum":
-        answer.sort(key=lambda x: x[2])
-    elif sort_by == "remain":
-        answer.sort(key=lambda x: x[3])
-
-    return answer
-
-
-def solution(data, ext, val_ext, sort_by):
-    index_map = {
-        "code" : 0,
-        "data" : 1,
-        "maximum" : 2,
-        "remain" : 3
-    }
-
-    idx_ext = index_map[ext]
-    idx_sort = index_map[sort_by]
-
-    answer = [d for d in data if data[idx_ext] < val_ext]
-    answer.sort(key = lambda x: x[idx_sort])
-
+    ext_index = table_map.get(ext)
+    sortby_index = table_map.get(sort_by)
+    
+    
+    answer = [d for d in data if d[ext_index] < val_ext]
+    
+    answer.sort(key = lambda x: x[sortby_index])
+    
     return answer
