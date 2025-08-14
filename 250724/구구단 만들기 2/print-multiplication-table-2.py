b91fd363a38f6417edd4da670e2574afecd04cd4
leah-1ee <dltjddms1028@gmail.com>
2025-08-14 17:54:01 +0900

    Delete 250724 directory

--- 250724/구구단 만들기 2/print-multiplication-table-2.py
+++
@@ -1,9 +0,0 @@
-a, b = map(int, input().split())
-
-for i in range(2, 9, 2):
-    for j in range(b, a-1, -1):
-        print(f"{j} * {i} = {j*i}", end="")
-        if (j!=a):
-            print(" / ", end="")
-
-    print()
--- 250724/정사각 알파벳 출력하기/print-alpabet-in-square.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-k=0
-
-for i in range(n):
-    for j in range(n):
-        print(chr(65+k), end="")
-        k+=1
-    print()
