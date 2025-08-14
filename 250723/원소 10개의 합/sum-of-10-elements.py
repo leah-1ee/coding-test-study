7d3a0db6186775096d05b80a050d59bae5ec661c
leah-1ee <dltjddms1028@gmail.com>
2025-08-14 17:54:15 +0900

    Delete 250723 directory

--- 250723/원소 10개의 합/sum-of-10-elements.py
+++
@@ -1,6 +0,0 @@
-n=list(map(int, input().split()))
-s=0
-
-for i in range(10):
-    s += n[i]
-print(s)
--- 250723/정사각형 출력/print-square.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-k = 1
-
-for i in range(n):
-    for j in range(n):
-        print(k, end=" ")
-        k += 1
-    print()
