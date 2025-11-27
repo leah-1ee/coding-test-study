1abd3b57f33137b2ee2b7b404748677f0c97428b
leah-1ee <dltjddms1028@gmail.com>
2025-11-27 17:06:54 +0900

    Title: 최고의 13위치 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251127/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,26 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+# 최종 합 변수 
+max_sum = 0
+
+# 첫 번째 격자 위치 
+for i in range(n):
+    for j in range(n-2):
+        coin1 = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        # 격자에 동전이 있으면 
+        if coin1:
+            # 두 번째 격자 위치 
+            for k in range(n):
+                for l in range(n-2):
+                    # 격자 중복 방지 
+                    if k == i and j-2 <= l <= j+2:
+                        continue
+                    else:
+                        coin2 = arr[k][l] + arr[k][l+1] + arr[k][l+2]
+                    # 격자에 동전이 있으면 
+                    if coin2:
+                        max_sum = max(max_sum, coin1 + coin2)
+
+print(max_sum)
