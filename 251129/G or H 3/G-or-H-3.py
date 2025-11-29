b8331bab69ac84f167f4950efb52a36813f2dd28
leah-1ee <dltjddms1028@gmail.com>
2025-11-29 15:13:57 +0900

    Title: G or H 3, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251129/G or H 3/G-or-H-3.py
@@ -0,0 +1,29 @@
+n, k = map(int, input().split()) # n 명, k 크기 
+x = [] # 위치
+c = [] # 알파벳 
+for _ in range(n):
+    pos, char = input().split()
+    x.append(int(pos))
+    c.append(char)
+
+# Please write your code here.
+# 위치에 따른 알파벳 기록할 배열 
+arr = [0] * (max(x)+1)
+
+# 알파벳 기록 
+for i in range(len(x)):
+    arr[x[i]] = c[i]
+
+
+max_cnt = 0
+# 최대 점수 구하기 
+for i in range(len(arr)-k):
+    cnt = 0
+    for j in range(i, i+k+1):
+        if arr[j] == 'G':
+            cnt += 1
+        elif arr[j] == 'H':
+            cnt += 2
+    max_cnt = max(max_cnt, cnt)
+
+print(max_cnt)
