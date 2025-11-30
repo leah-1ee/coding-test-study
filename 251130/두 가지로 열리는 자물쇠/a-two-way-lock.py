90bfea52a91d6320bf034a9996a4944419dc3c70
leah-1ee <dltjddms1028@gmail.com>
2025-11-30 22:53:36 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 108ms, Memory: 17MB, Status: Passed - Codetree

--- 251130/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 251130/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -3,31 +3,30 @@ a1, b1, c1 = map(int, input().split())
 a2, b2, c2 = map(int, input().split())
 
 # Please write your code here.
-
+# 범위 set으로 만들기 
 a1, b1, c1 = a1+N, b1+N, c1+N
 a2, b2, c2 = a2+N, b2+N, c2+N
-a1_scope = [i%N for i in range(a1-2, a1+3)]
-a2_scope = [i%N for i in range(a2-2, a2+3)]
-b1_scope = [i%N for i in range(b1-2, b1+3)]
-b2_scope = [i%N for i in range(b2-2, b2+3)]
-c1_scope = [i%N for i in range(c1-2, c1+3)]
-c2_scope = [i%N for i in range(c2-2, c2+3)]
-
-a_set = set(a1_scope + a2_scope)
-b_set = set(b1_scope + b2_scope)
-c_set = set(c1_scope + c2_scope)
-
-a_diff = len(a1_scope + a2_scope) - len(a_set)
-b_diff = len(b1_scope + b2_scope) - len(b_set)
-c_diff = len(c1_scope + c2_scope) - len(c_set)
-
-diff = a_diff * b_diff * c_diff
-
-if N >= 5:
-    test = 250 - diff 
-    
-else:
-    test = (N ** 3) - diff
+a1_scope = set(i%N for i in range(a1-2, a1+3))
+a2_scope = set(i%N for i in range(a2-2, a2+3))
+b1_scope = set(i%N for i in range(b1-2, b1+3))
+b2_scope = set(i%N for i in range(b2-2, b2+3))
+c1_scope = set(i%N for i in range(c1-2, c1+3))
+c2_scope = set(i%N for i in range(c2-2, c2+3))
 
 
-print(test)
+# 조합 수 저장
+cnt = 0
+
+# 모든 조합 탐색 
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        for k in range(1, N+1):
+            ii, jj, kk = i % N, j % N, k % N
+            # 조합 1, 2에 대하여 각각 열리는지 확인 
+            key1 = (ii in a1_scope) and (jj in b1_scope) and (kk in c1_scope)
+            key2 = (ii in a2_scope) and (jj in b2_scope) and (kk in c2_scope)
+            # 두 조합 중 하나라도 만족하면 cnt + 1
+            if key1 or key2:
+                cnt += 1
+
+print(cnt)
