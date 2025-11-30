0081db7f932ce2fcb3a1b87d546c897dd108e68f
leah-1ee <dltjddms1028@gmail.com>
2025-11-30 21:58:23 +0900

    Title: 개발자의 능력 3, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251130/개발자의 능력 3/ability-of-developer-3.py
+++ 251130/개발자의 능력 3/ability-of-developer-3.py
@@ -1,17 +1,21 @@
 abilities = list(map(int, input().split()))
 
 # Please write your code here.
-
+# 개발자 수, 능력 총 합, 차이 최솟값 저장 
 n = len(abilities)
 total = sum(abilities)
 min_diff = float('inf')
 
+# 개발자 3명 조합 
 for i in range(n-2):
-    for j in range(1, n-1):
-        for k in range(2, n):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            # g1, g2 능력 합 계산 
             group1 = abilities[i] + abilities[j] + abilities[k]
             group2 = total - group1
+            # 차이 계산, 최솟값 업데이트 
             diff = abs(group1 - group2)
             min_diff = min(min_diff, diff)
+
 
 print(min_diff)
