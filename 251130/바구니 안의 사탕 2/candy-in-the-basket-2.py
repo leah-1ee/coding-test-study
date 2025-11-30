b5f8b18ddd0d4ddea05944de84fc554908a8c9ac
leah-1ee <dltjddms1028@gmail.com>
2025-11-30 17:17:06 +0900

    Title: 바구니 안의 사탕 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251130/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 251130/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -7,17 +7,28 @@ for _ in range(N):
     candy.append(c)
     pos.append(p)
 
-# Please write your code here.
-arr = [0] * (max(pos)+1)
+# 최대 사탕, 현재 사탕 합, 위치 별 사탕 수 저장할 배열 
 max_candy = 0
+current_candy = 0
+arr = [0] * (max(pos) + 1)
 
-for i in range(N):
-    arr[pos[i]] = candy[i]
+# 배열에 사탕 수 기록 
+for i in range(len(pos)):
+    arr[pos[i]] += candy[i]
 
-for c in range(K, len(arr)-K):
-    total = 0
-    for j in range(c-K, c+K+1):
-        total += arr[j]
-    max_candy = max(max_candy, total)
+# 윈도우 크기
+window = 2 * K + 1
+
+# 초기 윈도우 사탕 수 구하기 
+limit = min(len(arr), window)
+for i in range(limit):
+    current_candy += arr[i]
+max_candy = current_candy
+
+# 윈도우를 한 칸씩 이동하면서 사탕 수 계산 
+for i in range(window, len(arr)):
+    current_candy += arr[i]
+    current_candy -= arr[i-window]
+    max_candy = max(max_candy, current_candy) 
 
 print(max_candy)
