f9aba9749b70db56d77bd257cd770c8af8ed59a6
leah-1ee <dltjddms1028@gmail.com>
2025-11-30 18:02:44 +0900

    Title: 밭의 높이를 고르게하기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 251130/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 251130/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -4,13 +4,16 @@ arr = list(map(int, input().split()))
 # Please write your code here.
 # 연속 T번 높이 H
 
-
+# 최소 비용 저장 
 min_cost = float('inf')
 
-for i in range(len(arr)-T):
+# 시작 위치 결정 
+for i in range(len(arr)-T+1):
     cost = 0
+    # i ~ i + T 순회, 높이 H 맞추기 위한 비용 계산 
     for j in range(i, i+T):
         cost += abs(arr[j]-H)
+    # 최소 비용 업데이트 
     min_cost = min(min_cost, cost)
 
 print(min_cost)
