565494bfd1cd56e811c4972d8d2a68278bdfde4f
leah-1ee <dltjddms1028@gmail.com>
2025-12-04 11:00:21 +0900

    [level 1] Title: 최소직사각형, Time: 10.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,17 +1,17 @@
 def solution(sizes):
     
-    # 지갑 가로, 세로 길이 
-    wallet_w = 0
-    wallet_h = 0
+    # 가로, 세로 사이즈 저장 
+    w_wallet = 0
+    h_wallet = 0
     
-    # 모든 크기에 대해서 긴 쪽 -> 가로, 짧은 쪽 -> 세로 로 회전 
+    # 모든 명함에 대해서 반복 
     for w, h in sizes:
+        # 더 큰 길이가 가로, 더 짧은 길이가 세로가 되게 돌림 
         big = max(w, h)
         small = min(w, h)
         
-        # 모든 크기에 대해 가장 긴 가로와 가장 긴 세로 찾기 
-        wallet_w = max(wallet_w, big)
-        wallet_h = max(wallet_h, small)
+        w_wallet = max(w_wallet, big)
+        h_wallet = max(h_wallet, small)
         
     # 지갑 크기 리턴 
-    return wallet_w * wallet_h 
+    return w_wallet * h_wallet 
