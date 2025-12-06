bf77de5ab6dad8225dab0d73180ed5926eaf5165
leah-1ee <dltjddms1028@gmail.com>
2025-12-07 08:40:21 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.88 ms, Memory: 9.28 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,20 @@
+def solution(k, scores):
+    # 최하위 점수 기록 배열, 명예의 전당 배열 
+    answer = []
+    honor = []
+    
+    for score in scores:
+        # 명예의 전당 목록 채우기 
+        if len(answer) < k:
+            honor.append(score)
+            
+        else:
+            # 더 높은 점수가 나오면 
+            if score > min(honor):
+                # 기존 점수 삭제, 새 점수 기록 
+                honor.pop(honor.index(min(honor)))
+                honor.append(score)
+            
+        # 최하위 점수 기록 
+        answer.append(min(honor))
+    return answer
