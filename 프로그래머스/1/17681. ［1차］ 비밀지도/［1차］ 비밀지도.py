bc8cc82ad11d12eeec89c352fd7057709bc74629
leah-1ee <dltjddms1028@gmail.com>
2025-12-07 09:52:37 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -4,32 +4,21 @@
 # 이진수 -> 10진수로 암호화 
 # 공백, # 구성 문자열 배열 출력 
 
-# 10진수 -> 2진수 문자열
-def from_decimal(num):
-    binary = ""
-    while num > 0:
-        binary = str(num % 2) + binary
-        num //= 2
-    return binary
-
 
 def solution(n, arr1, arr2):
     # 정답 배열 
     answer = []
     
-    # 암호 해독 
     for i in range(n):
-        # 10진수 암호화 -> 2진수 
-        binary1 = from_decimal(arr1[i]).zfill(n)
-        binary2 = from_decimal(arr2[i]).zfill(n)
-        secret_code = ''
-        
-        # 공통으로 0 -> 공백, 아니면 벽(#)
-        for j in range(n):
-            if binary1[j] == '0' and binary2[j] == '0':
-                secret_code += ' '
-            else:
-                secret_code += '#'
-        answer.append(secret_code)
+        # 비트 연산으로 지도 합치기
+        merged_map = arr1[i] | arr2[i]
+
+        # 2진수 문자열로 변환, 0b 접두어 자르기, n자리수 맞추기 
+        binary_str = bin(merged_map)[2:].zfill(n)
+
+        # 0 -> 공백, 1 -> #으로 교체
+        decoded_line = binary_str.replace('0', ' ').replace('1', '#')
+
+        answer.append(decoded_line)
     
     return answer
