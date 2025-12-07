b1a35d48cb1f822b34ca7f3fa38a244844d75956
leah-1ee <dltjddms1028@gmail.com>
2025-12-07 12:49:53 +0900

    [level 1] Title: 모의고사, Time: 4.93 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,31 +1,26 @@
 def solution(answers):
-    answer = []
     
-    n = (len(answers) // 5) 
+    # 찍기 패턴 
+    patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
     
-    person1 = ([1,2,3,4,5] * (len(answers) // 5 + 1))[:len(answers)]
-    person2 = ([2, 1, 2, 3, 2, 4, 2, 5] * (len(answers) // 8 + 1))[:len(answers)]
-    person3 = ([3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (len(answers) // 10 + 1))[:len(answers)]
+    # 점수 
+    scores = [0, 0, 0]
     
-    cnt1, cnt2, cnt3 = 0,0,0
-    for i in range(len(answers)):
+    # 점수 계산 
+    # i: 정답인덱스, answer: 정답 번호 
+    for i, answer in enumerate(answers):
         
-        
-        if person1[i] == answers[i]:
-            cnt1 += 1
-        if person2[i] == answers[i]:
-            cnt2 += 1
-        if person3[i] == answers[i]:
-            cnt3 += 1
-        
-      
-    max_val = max(cnt1, cnt2, cnt3)
-    
-    if max_val == cnt1:
-            answer.append(1)
-    if max_val == cnt2:
-            answer.append(2)
-    if max_val == cnt3:
-            answer.append(3)
-
-    return sorted(answer)
+        # 패턴 하나씩 탐색 
+        # j: 패턴 번호, pattern: 패턴 리스트 
+        for j, pattern in enumerate(patterns):
+            
+            # 정답 인덱스(i)가 패턴 길이를 넘어가면 다시 패턴 0번 인덱스부터 시작
+            if answer == pattern[i % len(pattern)]:
+                scores[j] += 1
+                
+    # 최다 정답자 번호 (1번부터 시작) 
+    return [i+1 for i, score in enumerate(scores) if score == max(scores)]
