70d34cc8d09599035c8bb7cc50ed677665673e54
leah-1ee <dltjddms1028@gmail.com>
2025-12-08 16:09:26 +0900

    [level 1] Title: 소수 찾기, Time: 3030.77 ms, Memory: 9.57 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,21 @@
+# 소수 판별 함수 
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    
+    return True
+    
+
+def solution(n):
+    answer = 0
+    
+    for i in range(2, n+1):
+        if is_prime(i):
+            answer += 1
+        
+    
+    return answer
