311a700e518f2c3f74dbb863c516721c791fd536
Seongeun Lee <dltjddms1028@gmail.com>
2026-01-01 16:02:17 +0900

    [level 1] Title: 실패율, Time: 2004.83 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -5,26 +5,24 @@ def solution(N, stages):
     # 스테이지 별 실패율 저장 
     stage_dict = {}
     
+    # 전체 플레이어 수
+    total_player = len(stages)
+    
     for i in range(1, N+1):
         # 실패율 구하기 
-        player = 0
-        not_clear = 0
-        
-        for stage in stages:
-            # 스테이지에 도달한 플레이어 수
-            if i <= stage:
-                player += 1
+        if total_player:
             # 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수
-            if i == stage:
-                not_clear += 1
+            player = stages.count(i)
+            # 실패율 계산 
+            fail_rate = player / total_player
+            # 다음 스테이지에 도달한 플레이어 수 = 현재 인원 - 현재 스테이지에 멈춘 플레이어 수 
+            total_player -= player
         
         # 스테이지에 도달한 유저가 없는 경우 해당 스테이지의 실패율은 0
-        if player:
-            fail_rate = not_clear/player 
         else:
             fail_rate = 0
-        
-        # 딕셔너리 저장
+            
+        # 스테이지 번호 : 실패율 저장 
         stage_dict[i] = fail_rate
         
     # 정렬 기준: 1. 실패율 높은 순, 2. 스테이지 번호 작은 순 
