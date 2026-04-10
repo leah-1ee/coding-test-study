87df4ceb1cc6d2b5c10623c5383aa59efefc7985
Seongeun Lee <dltjddms1028@gmail.com>
2026-04-10 23:37:22 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 772 ms, Memory: 127416 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,90 +1,100 @@
-def dust_count(N, M, grid):
+"""
+7 8 3
+0 0 0 0 0 0 0 9
+0 0 0 0 3 0 0 8
+-1 0 5 0 0 0 22 0
+-1 8 0 0 0 0 0 0
+0 0 0 0 0 10 43 0
+0 0 5 0 15 0 0 0
+0 0 40 0 0 0 20 0
+"""
+
+def count_grid(N, M, grid):
   cnt = 0
+  
   for i in range(N):
-      for j in range(M):
-          if grid[i][j] > 0:
-              cnt += grid[i][j]
+    for j in range(M):
+      if grid[i][j] > 0:
+        cnt += grid[i][j]
+        
   return cnt
 
-def simulate(N, M, grid, cleaner, T):
-  # 상, 하, 좌, 우
-  dr_spread = [-1, 1, 0, 0]
-  dc_spread = [0, 0, -1, 1]
-
-  for _ in range(T):
-      # 1. 미세먼지 확산
-      new_grid = [row[:] for row in grid]
-      for i in range(N):
-          for j in range(M):
-              if grid[i][j] > 0:
-                  val = grid[i][j]
-                  spread_amount = val // 5
-                  spread_cnt = 0
-
-                  for d in range(4):
-                      nr, nc = i + dr_spread[d], j + dc_spread[d]
-                      # 공기청정기가 아닌 빈 칸으로만 확산
-                      if 0 <= nr < N and 0 <= nc < M and grid[nr][nc] != -1:
-                          new_grid[nr][nc] += spread_amount
-                          spread_cnt += 1
-
-                  # 원래 칸에서는 확산된 총량만큼 한 번에 빼기
-                  new_grid[i][j] -= (spread_amount * spread_cnt)
-
-      grid = new_grid
-
-      # 2. 공기청정기 가동
-      r1, c1 = cleaner[0]
-      r2, c2 = cleaner[1]
-      grid = clean(r1, c1, 0, grid, N, M)
-      grid = clean(r2, c2, 1, grid, N, M)
-
-  return dust_count(N, M, grid)
-
-def clean(start_r, start_c, n, grid, N, M):
-  # n == 0: 위쪽 청정기 (반시계) / 우, 상, 좌, 하
+def clean(start_r, start_c, n, N, M, grid):
   if n == 0:
-      dr = [0, -1, 0, 1]
-      dc = [1, 0, -1, 0]
-      min_r, max_r = 0, start_r
-  # n == 1: 아래쪽 청정기 (시계) / 우, 하, 좌, 상
+    dr = [0, -1, 0, 1]
+    dc = [1, 0, -1, 0]
+    min_r, max_r = 0, start_r
+
   else:
-      dr = [0, 1, 0, -1]
-      dc = [1, 0, -1, 0]
-      min_r, max_r = start_r, N - 1
+    dr = [0, 1, 0, -1]
+    dc = [1, 0, -1, 0]
+    min_r, max_r = start_r, N-1
+
 
   d = 0
   r, c = start_r + dr[d], start_c + dc[d]
-  prev_value = 0  # 공기청정기에서 갓 나온 맑은 공기 (0)
+  prev_val = 0
+  
+  while(True):
+    if grid[r][c] == -1:
+      return grid
 
-  while True:
-      # 공기청정기 위치로 다시 돌아오면 종료 (먼지 소멸)
-      if grid[r][c] == -1:
-          break
+    temp = grid[r][c]
+    grid[r][c] = prev_val
+    prev_val = temp
 
-      # 먼지를 밀어내는 핵심 로직 (Swap X, Temp O)
-      temp = grid[r][c]
-      grid[r][c] = prev_value
-      prev_value = temp
-
+    nr, nc = r + dr[d], c + dc[d]
+    if not (min_r <= nr <= max_r and 0 <= nc < M):
+      d = (d+1) % 4
       nr, nc = r + dr[d], c + dc[d]
 
-      # 지정된 위/아래 구역을 벗어나면 방향을 꺾음
-      if not (min_r <= nr <= max_r and 0 <= nc < M):
-          d = (d + 1) % 4
-          nr, nc = r + dr[d], c + dc[d]
+    r, c = nr, nc
+  
 
-      r, c = nr, nc
+def simulation(N, M, T, grid, cleaner):
+  dr = [-1, 1, 0, 0]
+  dc = [0, 0, -1, 1]
+  
+  for _ in range(T):
+    new_grid = [row[:] for row in grid]
+    
+    for r in range(N):
+      for c in range(M):
+        
+        if new_grid[r][c] > 0:
+          spread_cnt = 0
+          val = grid[r][c] // 5
+          
+          for d in range(4):
+            nr = r + dr[d]
+            nc = c + dc[d]
+            
+            if 0 <= nr < N and 0 <= nc < M and (nr, nc) not in cleaner:
+              spread_cnt += 1
+              new_grid[nr][nc] += val
+              
+          new_grid[r][c] -= (val * spread_cnt)
 
-  return grid
+    grid = new_grid
 
-# --- 입력 및 실행 ---
+    r1, c1 = cleaner[0]
+    r2, c2 = cleaner[1]
+    grid = clean(r1, c1, 0, N, M, grid)
+    grid = clean(r2, c2, 1, N, M, grid)
+
+  return count_grid(N, M, grid)
+            
+
 N, M, T = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(N)]
+
+grid = []
+for _ in range(N):
+  row = list(map(int, input().split()))
+  grid.append(row)
 
 cleaner = []
 for i in range(N):
   if grid[i][0] == -1:
-      cleaner.append((i, 0))
+    cleaner.append((i, 0))
 
-print(simulate(N, M, grid, cleaner, T))
+print(simulation(N, M, T, grid, cleaner))
