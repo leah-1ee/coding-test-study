70c0bc89463b7248c22796c6ee215274fa4d3e23
Seongeun Lee <dltjddms1028@gmail.com>
2026-05-22 16:51:05 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,16 +1,14 @@
 def solution(n, lost, reserve):
-    # 본인이 도난당한 경우 제외 
-    real_lost = set(lost) - set(reserve)
-    real_reserve = set(reserve) - set(lost)
     
-    for person in sorted(real_reserve):
-        # 전 번호가 도난당함
-        if person-1 in real_lost:
-            real_lost.remove(person-1)
-
-        # 뒤 번호가 도난당함 
-        elif person+1 in real_lost:
-            real_lost.remove(person+1)
-        
-    # 전체 인원 - 체육복을 빌리지 못한 사람 수 
-    return n - len(real_lost)
+    both = set(lost) & set(reserve)
+    
+    lost = set(lost) - both
+    reserve = set(reserve) - both
+    
+    for person in sorted(reserve):
+        if person - 1 in lost:
+            lost.remove(person - 1)
+        elif person + 1 in lost:
+            lost.remove(person + 1)
+    
+    return n - len(lost)
