70d601be7489869da7f40bf1e7511673903f6cb7
Seongeun Lee <dltjddms1028@gmail.com>
2026-08-04 15:43:47 +0900

    [D5] Title: 오! 나의 여신님, Time: 32 ms, Memory: 5,856 KB -BaekjoonHub

--- SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.cpp
+++ SWEA/D5/7793. 오！ 나의 여신님/오！ 나의 여신님.cpp
@@ -12,6 +12,97 @@ struct Node
 	int time;
 };
 
+const int dr[4] = { -1,1,0,0 };
+const int dc[4] = { 0,0,-1,1 };
+
+void spreadDemon(
+	queue<Node>& demon,
+	vector<vector<bool>>& hand,
+	const vector<string> grid,
+	int n,
+	int m,
+	int curTime
+	) 
+{
+	while (!demon.empty() && demon.front().time == curTime) {
+		Node cur = demon.front();
+		demon.pop();
+
+		for (int d = 0; d < 4; d++) {
+			int nr = cur.r + dr[d];
+			int nc = cur.c + dc[d];
+
+			bool inRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
+			if (!inRange || hand[nr][nc]) continue;
+			if (grid[nr][nc] != '.' && grid[nr][nc] != 'S') continue;
+			
+			hand[nr][nc] = true;
+			demon.push({ nr, nc, curTime + 1 });
+		}
+	}
+}
+
+int bfs() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<string> grid(n);
+	for (int i = 0; i < n; i++) {
+		cin >> grid[i];
+	}
+
+	int startR = -1, startC = -1, endR = -1, endC = -1;
+	vector<vector<bool>> visited(n, vector<bool>(m, false));
+	vector<vector<bool>> hand(n, vector<bool>(m, false));
+
+	queue<Node> demon;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (grid[i][j] == 'S') {
+				startR = i;
+				startC = j;
+			}
+			if (grid[i][j] == 'D') {
+				endR = i;
+				endC = j;
+			}
+			if (grid[i][j] == '*') {
+				demon.push({ i,j,0 });
+				hand[i][j] = true;
+			}
+		}
+	}
+
+	queue<Node> q;
+	q.push({ startR, startC, 0 });
+	visited[startR][startC] = true;
+
+	while (!q.empty()) {
+		Node cur = q.front();
+		q.pop();
+
+		spreadDemon(demon, hand, grid, n, m, cur.time);
+
+		if (cur.r == endR && cur.c == endC) {
+			return cur.time;
+		}
+
+		for (int d = 0; d < 4; d++) {
+			int nr = cur.r + dr[d];
+			int nc = cur.c + dc[d];
+
+			bool inRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
+			if (!inRange || visited[nr][nc] || hand[nr][nc]) continue; 
+			if (grid[nr][nc] != '.' && grid[nr][nc] != 'D') continue;
+			visited[nr][nc] = true;
+			q.push({ nr, nc, cur.time + 1 });
+		}
+	}
+
+	return -1;
+}
+
 int main(int argc, char** argv)
 {
 	int test_case;
@@ -20,89 +111,7 @@ int main(int argc, char** argv)
 	
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-		int n, m;
-		cin >> n >> m;
-
-		vector<string> grid(n);
-		for (int i = 0; i < n; i++) {
-			cin >> grid[i];
-		}
-
-		int answer = -1;
-
-		vector<vector<bool>> visited(n, vector<bool>(m, false));
-		vector<vector<bool>> hand(n, vector<bool>(m, false));
-
-		int startR, startC, endR, endC;
-
-		int dr[4] = { -1,1,0,0 };
-		int dc[4] = { 0,0,-1,1 };
-
-		queue<Node> q;
-		queue<Node> demon;
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (grid[i][j] == 'S') {
-					startR = i;
-					startC = j;
-				}
-				if (grid[i][j] == 'D') {
-					endR = i;
-					endC = j;
-				}
-				if (grid[i][j] == '*') {
-					demon.push({ i,j,0 });
-					hand[i][j] = true;
-				}
-			}
-		}
-
-		q.push({ startR, startC, 0 });
-		visited[startR][startC] = true;
-
-		while (!q.empty()) {
-			int r = q.front().r;
-			int c = q.front().c;
-			int time = q.front().time;
-			q.pop();
-
-
-			while (!demon.empty()) {
-				int dmr = demon.front().r;
-				int dmc = demon.front().c;
-				int dmt = demon.front().time;
-
-				if (dmt != time) break;
-
-				demon.pop();
-
-				for (int d = 0; d < 4; d++) {
-					int ndmr = dmr + dr[d];
-					int ndmc = dmc + dc[d];
-
-					if (0 <= ndmr && ndmr < n && 0 <= ndmc && ndmc < m && !hand[ndmr][ndmc] && (grid[ndmr][ndmc] == '.' || grid[ndmr][ndmc] == 'S')) {
-						demon.push({ ndmr, ndmc, dmt + 1 });
-						hand[ndmr][ndmc] = true;
-					}
-				}
-			}
-
-			if (r == endR && c == endC) {
-				answer = time;
-				break;
-			}
-
-			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-
-				if (0 <= nr && nr < n && 0 <= nc && nc < m && !visited[nr][nc] && (grid[nr][nc] == '.' || grid[nr][nc] == 'D') && !hand[nr][nc]) {
-					q.push({ nr, nc, time + 1 });
-					visited[nr][nc] = true;
-				}
-			}
-		}
+		int answer = bfs();
 
 		if (answer >= 0) cout << "#" << test_case << " " << answer <<"\n";
 		else cout << "#" << test_case << " GAME OVER\n";
