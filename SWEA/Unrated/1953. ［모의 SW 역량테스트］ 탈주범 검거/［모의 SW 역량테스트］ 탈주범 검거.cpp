8fd8fdf4cbfd7d811dfeabc38aeeddfbcf961723
Seongeun Lee <dltjddms1028@gmail.com>
2026-08-05 10:20:58 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 17 ms, Memory: 5,860 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.cpp
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.cpp
@@ -1,234 +1,73 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
 using namespace std;
 
-struct Node
-{
-	int r;
-	int c;
-	int time;
+// 방향: 0=상,1=하,2=좌,3=우
+const int dr[4] = { -1, 1, 0, 0 };
+const int dc[4] = { 0, 0, -1, 1 };
+
+enum Dir { UP = 1, DOWN = 2, LEFT = 4, RIGHT = 8 };
+const int dirBit[4] = { UP, DOWN, LEFT, RIGHT };
+const int oppBit[4] = { DOWN, UP, RIGHT, LEFT }; // 이웃이 나와 연결되려면 반대 방향이 열려있어야 함
+
+// 파이프 타입별 연결 방향 (0번 인덱스는 사용 안 함: type 0 = 벽)
+const int connMask[8] = {
+	0,                    // 0: 벽
+	UP | DOWN | LEFT | RIGHT, // 1: 십자
+	UP | DOWN,            // 2: 상하
+	LEFT | RIGHT,         // 3: 좌우
+	UP | RIGHT,           // 4: 상우
+	DOWN | RIGHT,         // 5: 하우
+	DOWN | LEFT,          // 6: 하좌
+	UP | LEFT             // 7: 상좌
 };
 
-// 상하좌우
-int dr[4] = { -1,1,0,0 };
-int dc[4] = { 0,0,-1,1 };
+struct Node { int r, c, time; };
 
 int bfs() {
 	int n, m, startR, startC, L;
 	cin >> n >> m >> startR >> startC >> L;
 
 	vector<vector<int>> grid(n, vector<int>(m));
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> grid[i][j];
-		}
-	}
-
-	queue<Node> q;
-	q.push({ startR, startC, 1 });
+	for (auto& row : grid)
+		for (auto& v : row) cin >> v;
 
 	vector<vector<bool>> visited(n, vector<bool>(m, false));
+	queue<Node> q;
+	q.push({ startR, startC, 1 });
 	visited[startR][startC] = true;
-
 	int cnt = 1;
 
 	while (!q.empty()) {
-		int r = q.front().r;
-		int c = q.front().c;
-		int t = q.front().time;
-		q.pop();
-
-		// cout << r << " " << c << " " << t << "\n";
-
-		int type = grid[r][c];
-
+		auto[r, c, t] = q.front(); q.pop();
 		if (t == L) break;
 
-		if (type == 0) continue;
+		int curMask = connMask[grid[r][c]];
+		if (curMask == 0) continue;
 
-		int nr, nc;
+		for (int d = 0; d < 4; d++) {
+			if (!(curMask & dirBit[d])) continue; // 현재 타입이 이 방향으로 안 열림
 
-		// 상하좌우
-		if (type == 1) {
-			for (int d = 0; d < 4; d++) {
-				nr = r + dr[d];
-				nc = c + dc[d];
+			int nr = r + dr[d], nc = c + dc[d];
+			if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+			if (visited[nr][nc]) continue;
 
-				bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
-				if (isRange  && !visited[nr][nc]) {
-					int ntype = grid[nr][nc];
-					if (ntype == 0) continue;
+			int ntype = grid[nr][nc];
+			if (!(connMask[ntype] & oppBit[d])) continue; // 이웃이 반대 방향으로 안 열림
 
-					if (d == 0 && !(ntype == 1 || ntype == 2 || ntype == 5 || ntype == 6)) continue;
-					if (d == 1 && !(ntype == 1 || ntype == 2 || ntype == 4 || ntype == 7)) continue;
-					if (d == 2 && !(ntype == 1 || ntype == 3 || ntype == 4 || ntype == 5)) continue;
-					if (d == 3 && !(ntype == 1 || ntype == 3 || ntype == 6 || ntype == 7)) continue;
-
-					cnt++;
-					q.push({ nr, nc, t + 1 });
-					visited[nr][nc] = true;
-				}
-			}
+			visited[nr][nc] = true;
+			cnt++;
+			q.push({ nr, nc, t + 1 });
 		}
-		// 상하 01
-		else if (type == 2) {
-			for (int d = 0; d < 2; d++) {
-				nr = r + dr[d];
-				nc = c + dc[d];
-
-				bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
-				if (isRange  && !visited[nr][nc]) {
-					int ntype = grid[nr][nc];
-					if (ntype == 0) continue;
-
-					if (d == 0 && !(ntype == 1 || ntype == 2 || ntype == 5 || ntype == 6)) continue;
-					if (d == 1 && !(ntype == 1 || ntype == 2 || ntype == 4 || ntype == 7)) continue;
-					if (d == 2 && !(ntype == 1 || ntype == 3 || ntype == 4 || ntype == 5)) continue;
-					if (d == 3 && !(ntype == 1 || ntype == 3 || ntype == 6 || ntype == 7)) continue;
-
-
-					cnt++;
-					q.push({ nr, nc, t + 1 });
-					visited[nr][nc] = true;
-				}
-			}
-		}
-
-		// 좌우 23
-		else if (type == 3) {
-			for (int d = 2; d < 4; d++) {
-				nr = r + dr[d];
-				nc = c + dc[d];
-
-				bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
-				if (isRange  && !visited[nr][nc]) {
-					int ntype = grid[nr][nc];
-					if (ntype == 0) continue;
-
-					if (d == 0 && !(ntype == 1 || ntype == 2 || ntype == 5 || ntype == 6)) continue;
-					if (d == 1 && !(ntype == 1 || ntype == 2 || ntype == 4 || ntype == 7)) continue;
-					if (d == 2 && !(ntype == 1 || ntype == 3 || ntype == 4 || ntype == 5)) continue;
-					if (d == 3 && !(ntype == 1 || ntype == 3 || ntype == 6 || ntype == 7)) continue;
-
-					cnt++;
-					q.push({ nr, nc, t + 1 });
-					visited[nr][nc] = true;
-				}
-			}
-		}
-
-		// 상우 03
-		else if (type == 4) {
-			for (int d = 0; d < 4; d += 3) {
-				nr = r + dr[d];
-				nc = c + dc[d];
-
-				bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
-				if (isRange  && !visited[nr][nc]) {
-					int ntype = grid[nr][nc];
-					if (ntype == 0) continue;
-
-					if (d == 0 && !(ntype == 1 || ntype == 2 || ntype == 5 || ntype == 6)) continue;
-					if (d == 1 && !(ntype == 1 || ntype == 2 || ntype == 4 || ntype == 7)) continue;
-					if (d == 2 && !(ntype == 1 || ntype == 3 || ntype == 4 || ntype == 5)) continue;
-					if (d == 3 && !(ntype == 1 || ntype == 3 || ntype == 6 || ntype == 7)) continue;
-
-					cnt++;
-					q.push({ nr, nc, t + 1 });
-					visited[nr][nc] = true;
-				}
-			}
-		}
-
-		// 하우 13
-		else if (type == 5) {
-			for (int d = 1; d < 4; d+=2) {
-				nr = r + dr[d];
-				nc = c + dc[d];
-
-				bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
-				if (isRange  && !visited[nr][nc]) {
-					int ntype = grid[nr][nc];
-					if (ntype == 0) continue;
-
-					if (d == 0 && !(ntype == 1 || ntype == 2 || ntype == 5 || ntype == 6)) continue;
-					if (d == 1 && !(ntype == 1 || ntype == 2 || ntype == 4 || ntype == 7)) continue;
-					if (d == 2 && !(ntype == 1 || ntype == 3 || ntype == 4 || ntype == 5)) continue;
-					if (d == 3 && !(ntype == 1 || ntype == 3 || ntype == 6 || ntype == 7)) continue;
-
-					cnt++;
-					q.push({ nr, nc, t + 1 });
-					visited[nr][nc] = true;
-				}
-			}
-		}
-
-		// 하좌 12
-		else if (type == 6) {
-			for (int d = 1; d < 3; d++) {
-				nr = r + dr[d];
-				nc = c + dc[d];
-
-				bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
-				if (isRange  && !visited[nr][nc]) {
-					int ntype = grid[nr][nc];
-					if (ntype == 0) continue;
-
-					if (d == 0 && !(ntype == 1 || ntype == 2 || ntype == 5 || ntype == 6)) continue;
-					if (d == 1 && !(ntype == 1 || ntype == 2 || ntype == 4 || ntype == 7)) continue;
-					if (d == 2 && !(ntype == 1 || ntype == 3 || ntype == 4 || ntype == 5)) continue;
-					if (d == 3 && !(ntype == 1 || ntype == 3 || ntype == 6 || ntype == 7)) continue;
-
-					cnt++;
-					q.push({ nr, nc, t + 1 });
-					visited[nr][nc] = true;
-				}
-			}
-		}
-
-		// 상좌 02
-		else if (type == 7) {
-			for (int d = 0; d < 4; d+=2) {
-				nr = r + dr[d];
-				nc = c + dc[d];
-
-				bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < m);
-				if (isRange  && !visited[nr][nc]) {
-					int ntype = grid[nr][nc];
-					if (ntype == 0) continue;
-
-					if (d == 0 && !(ntype == 1 || ntype == 2 || ntype == 5 || ntype == 6)) continue;
-					if (d == 1 && !(ntype == 1 || ntype == 2 || ntype == 4 || ntype == 7)) continue;
-					if (d == 2 && !(ntype == 1 || ntype == 3 || ntype == 4 || ntype == 5)) continue;
-					if (d == 3 && !(ntype == 1 || ntype == 3 || ntype == 6 || ntype == 7)) continue;
-
-					cnt++;
-					q.push({ nr, nc, t + 1 });
-					visited[nr][nc] = true;
-				}
-			}
-		}
-
-		
 	}
-
 	return cnt;
 }
 
-int main(int argc, char** argv)
-{
-	int test_case;
+int main() {
 	int T;
-
 	cin >> T;
-
-	for (test_case = 1; test_case <= T; ++test_case)
-	{
-
-		cout << "#" << test_case << " " << bfs() << "\n";
-
-	}
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+	for (int tc = 1; tc <= T; tc++)
+		cout << "#" << tc << " " << bfs() << "\n";
+	return 0;
 }
