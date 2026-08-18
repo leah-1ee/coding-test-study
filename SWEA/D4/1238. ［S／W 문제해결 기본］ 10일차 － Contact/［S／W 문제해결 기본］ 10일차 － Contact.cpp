514d00264b1c792dc20ad664295a53b6d07f0f9d
Seongeun Lee <selee3196@gmail.com>
2026-08-18 11:02:13 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 7 ms, Memory: 6,008 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.cpp
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.cpp
@@ -1,53 +1,64 @@
 #include<iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
+int n, start;
+vector<vector<bool>> graph;
+
+int bfs() {
+	queue<int> q;
+	q.push(start);
+
+	vector<bool> visited(101, false);
+	visited[start] = true;
+
+	int last;
+
+	while (!q.empty()) {
+		last = 0;
+		int size = q.size();
+
+		for (int i = 0; i < size; i++) {
+			
+			int num = q.front();
+			last = max(last, num);
+			q.pop();
+
+			for (int j = 0; j < 101; j++) {
+				if (graph[num][j] && !visited[j]) {
+					q.push(j);
+					visited[j] = true;
+				}
+			}
+		}
+	}
+
+	return last;
+}
+
 int main(int argc, char** argv)
 {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int test_case;
 	int T = 10;
 
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-
-		int n, start;
+		graph.assign(101, vector<bool>(101, false));
 		cin >> n >> start;
-
-		vector<vector<int>> graph(101);
-		vector<bool> visited(101, false);
-
+		
 		for (int i = 0; i < n/2; i++) {
-			int from, to;
-			cin >> from >> to;
-			graph[from].push_back(to);
+			int a, b;
+			cin >> a >> b;
+			graph[a][b] = true;
 		}
 
-		queue<int> q;
-		q.push(start);
-		visited[start] = true;
-
-		int answer;
-
-		while (!q.empty()) {
-			int last = 0;
-			int size = q.size();
-			for (int i = 0; i < size; i++) {
-				int cur = q.front();
-				q.pop();
-
-				if (cur > last) last = cur;
-
-				for (int next : graph[cur]) {
-					if (!visited[next]) {
-						visited[next] = true;
-						q.push(next);
-					}
-				}
-			}
-			answer = last;
-		}
+		int answer = bfs();
 
 		cout << "#" << test_case << " " << answer << "\n";
 
