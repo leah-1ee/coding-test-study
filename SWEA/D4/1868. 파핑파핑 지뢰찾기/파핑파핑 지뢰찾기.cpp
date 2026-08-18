14ea0d06a3609f7dcbfe4480a20ba52d4fb8787d
Seongeun Lee <selee3196@gmail.com>
2026-08-18 13:00:42 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 148 ms, Memory: 6,108 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.cpp
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.cpp
@@ -5,85 +5,88 @@
 
 using namespace std;
 
-// 좌상부터 시계방향: 좌상 상 우상 우 우하 하 좌하 좌
-int dr[8] = { -1,-1,-1,0,1,1,1,0 };
-int dc[8] = { -1,0,1,1,1,0,-1,-1 };
+// 상하좌우 좌상 우상 우하 좌하
+int dr[8] = { -1,1,0,0,-1,-1,1,1 };
+int dc[8] = { 0,0,-1,1,-1,1,1,-1 };
 
-// 8방향 . 인지?
 bool isZero(const vector<string>& grid, int r, int c, int n) {
 	for (int d = 0; d < 8; d++) {
 		int nr = r + dr[d];
 		int nc = c + dc[d];
-		bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < n);
 
+		bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < n);
 		if (!isRange) continue;
-		if (grid[nr][nc] != '.') return false;
+		if (grid[nr][nc] == '*')
+			return false;
 	}
 
 	return true;
 }
 
-// 0 주변 8칸 전부 연쇄적으로 열기 
-void unlock(
+void bfs(
 	const vector<string>& grid,
+	queue<pair<int, int>>& q,
 	vector<vector<bool>>& visited,
-	int i,
-	int j,
 	int n
-)
-{
-	int startR = i;
-	int startC = j;
-	queue<pair<int, int>> q;
-	q.emplace(startR, startC);
-	visited[startR][startC] = true;
+) {	
 
 	while (!q.empty()) {
-		auto cur = q.front();
+		int r = q.front().first;
+		int c = q.front().second;
 		q.pop();
 
 		for (int d = 0; d < 8; d++) {
-			int nr = cur.first + dr[d];
-			int nc = cur.second + dc[d];
+			int nr = r + dr[d];
+			int nc = c + dc[d];
 
 			bool isRange = (0 <= nr && nr < n && 0 <= nc && nc < n);
-			if (!isRange || grid[nr][nc] != '.' || visited[nr][nc]) continue;
-			visited[nr][nc] = true;
-
-			if(isZero(grid, nr, nc, n)) q.emplace(nr, nc);
-			
+			if (isRange && !visited[nr][nc] && grid[nr][nc] == '.') {
+				visited[nr][nc] = true;
+				if (isZero(grid, nr, nc, n)) {
+					q.push({ nr, nc });
+				}
+			}	
 		}
 	}
-
 }
 
-int bfs() {
+int simulation() {
 	int n;
 	cin >> n;
+
+	int cnt = 0;
+	vector<vector<bool>> visited(n, vector<bool>(n, false));
 
 	vector<string> grid(n);
 	for (int i = 0; i < n; i++) {
 		cin >> grid[i];
-	}
-
-	int cnt = 0;
-
-	vector<vector<bool>> visited(n, vector<bool>(n, false));
-	
-
-	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (visited[i][j] || grid[i][j] != '.') continue;
-			if (isZero(grid, i, j, n)) {
-				unlock(grid, visited, i, j, n);
-				cnt++;
-			} 
+			if (grid[i][j] == '*')
+				visited[i][j] = true;
 		}
 	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (!visited[i][j] && grid[i][j] == '.') cnt++;
+			if (!visited[i][j] && grid[i][j] == '.' && isZero(grid, i, j, n)) {
+
+				cnt++;
+				// 큐에 넣고 bfs
+				queue<pair<int, int>> q;
+				q.push({ i,j });
+				visited[i][j] = true;
+				// bfs
+				bfs(grid, q, visited, n);
+			}
+
+		}
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j])
+				cnt++;
 		}
 	}
 
@@ -101,7 +104,7 @@ int main(int argc, char** argv)
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 
-		int cnt = bfs();
+		int cnt = simulation();
 
 		cout << "#" << test_case << " " << cnt << "\n";
 
