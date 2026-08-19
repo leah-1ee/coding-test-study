d54f8d3afae13b287466eb61a9b0b01386b79cda
Seongeun Lee <selee3196@gmail.com>
2026-08-19 10:38:02 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 6 ms, Memory: 5,832 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.cpp
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.cpp
@@ -16,29 +16,26 @@ int main() {
 		cin >> dayCost >> monthCost >> threeCost >> yearCost;
 
 		vector<int> plan(13);
-
 		for (int i = 1; i < 13; i++) {
 			cin >> plan[i];
 		}
 
 		vector<int> dp(13, 0);
-
 		for (int i = 1; i < 13; i++) {
-			int useDay = dp[i - 1] + plan[i] * dayCost;
-			int useMonth = dp[i - 1] + monthCost;
+			int day = dp[i - 1] + plan[i] * dayCost;
+			int month = dp[i - 1] + monthCost;
 
-			dp[i] = min(useDay, useMonth);
+			dp[i] = min(day, month);
 
 			if (i >= 3) {
 				dp[i] = min(dp[i], dp[i - 3] + threeCost);
 			}
 		}
 
-		int answer = min(dp[12], yearCost);
+		dp[12] = min(dp[12], yearCost);
 
-		cout << "#" << test_case << " " << answer << "\n";
+		cout << "#" << test_case << " " << dp[12] << "\n";
 
 	}
-
 	return 0;
 }
